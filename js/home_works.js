9499404ca023b5117363d36e18e1054920b3dc35
NurDinBuy <nurik161103@gmail.com>
2023-06-14 20:04:51 +0600

    add lesson 3

--- js/home_works.js
+++ js/home_works.js
@@ -1,20 +1,82 @@
 // MOVE BLOCK
-
-const box = document.querySelector('.child_block')
+const childBlock = document.querySelector('.child_block')
 
 let positionX = 0
 let positionY = 0
 
 const move = () => {
-    if (positionX <= 448) {
-        positionX++
-        box.style.left = `${positionX}px`
-        setTimeout(move, 1)
-    } else if (positionY <= 449 && positionX >= 449) {
-        positionY++
-        box.style.top = `${positionY}px`
-        setTimeout(move, 1)
+    if (positionX < 449 && positionY === 0) {
+        positionX+=2
+        childBlock.style.left = `${positionX}px`
+        setTimeout(move, 10)
+    } else if (positionX >= 449 && positionY < 449) {
+        positionY+=2
+        childBlock.style.top = `${positionY}px`
+        setTimeout(move, 10)
+    } else if (positionX > 0 && positionY > 0) {
+        positionX-=2
+        childBlock.style.left = `${positionX}px`
+        setTimeout(move, 10)
+    } else if (positionX === 0 && positionY > 0) {
+        positionY-=2
+        childBlock.style.top = `${positionY}px`
+        setTimeout(move, 10)
     }
 }
 
 move()
+
+// STOPWATCH
+const minutesBlock = document.querySelector('#minutes'),
+    secondsBlock = document.querySelector('#seconds'),
+    mlSecondsBlock = document.querySelector('#ml-seconds'),
+    startButton = document.querySelector('#start'),
+    stopButton = document.querySelector('#stop'),
+    resetButton = document.querySelector('#reset')
+
+let interval
+let minutes = 0
+let seconds = 0
+let mlSeconds = 0
+
+const startTimer = () => {
+    mlSeconds++
+    mlSeconds <= 99 && (mlSecondsBlock.innerHTML = mlSeconds)
+    mlSeconds == 100 && (mlSecondsBlock.innerHTML = '00')
+
+    mlSecondsBlock.innerHTML = `0${mlSeconds}`
+    mlSeconds > 9 && (mlSecondsBlock.innerHTML = mlSeconds)
+    if (mlSeconds > 99) {
+        seconds++
+        secondsBlock.innerHTML = `0${seconds}`
+        mlSeconds = 0
+    }
+    seconds > 9 && (secondsBlock.innerHTML = seconds)
+    if (seconds > 59) {
+        minutes++
+        minutesBlock.innerHTML = `0${minutes}`
+        seconds = 0
+        secondsBlock.innerHTML = `0${seconds}`
+    }
+    minutes > 9 && (minutesBlock.innerHTML = minutes)
+}
+
+startButton.onclick = () => {
+    clearInterval(interval)
+    interval = setInterval(startTimer,  10)
+}
+
+stopButton.onclick = () => {
+    clearInterval(interval)
+}
+
+resetButton.onclick = () => {
+    clearInterval(interval)
+    minutes = 0
+    seconds = 0
+    mlSeconds = 0
+    minutesBlock.innerHTML = '00'
+    secondsBlock.innerHTML = '00'
+    mlSecondsBlock.innerHTML = '00'
+}
+
--- js/main.js
+++ js/main.js
@@ -57,7 +57,7 @@ const autoSlider = (i = 0) => {
         }
         hideSlide()
         showSlide(i)
-    }, 7000)
+    }, 10000)
 }
 
 next.onclick = () => {
@@ -73,3 +73,23 @@ prev.onclick = () => {
 }
 
 autoSlider(index)
+
+// MODAL
+
+const modal = document.querySelector('.modal')
+const modalTrigger = document.querySelector('#btn-get')
+const closeModalButton = document.querySelector('.modal_close')
+
+const openModal = () => {
+    modal.style.display = 'block'
+    document.body.style.overflow = 'hidden'
+}
+
+const closeModal = () => {
+    modal.style.display = 'none'
+    document.body.style.overflow = ''
+}
+
+modalTrigger.onclick = () => openModal()
+closeModalButton.onclick = () => closeModal()
+modal.onclick = (event) => event.target === modal && closeModal()
