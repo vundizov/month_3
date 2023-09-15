d67f2fdb6638c10c33fa0a4ccbac6bdd742c4ca5
1598256q <vundizovrustam07@gmail.com>
2023-09-16 00:09:16 +0600

    DZ

--- js/lesson.js
+++ js/lesson.js
@@ -14,3 +14,44 @@ phoneButton.onclick = () => {
         phoneResult.style.color = 'red'
     }
 }
+
+//TAB SLIDER
+const tabContendBlocks = document.querySelectorAll('.tab_content_block')
+const tabsParentBlock = document.querySelector('.tab_content_items')
+const tabsBlocks = document.querySelectorAll('.tab_content_item')
+
+
+
+const hideTabContend = () => {
+    tabContendBlocks.forEach(tabContendBlock=> {
+        tabContendBlock.style.display = 'none'
+    })
+    tabsBlocks.forEach(tabBlock => {
+        tabBlock.classList.remove('tab_content_item_active')
+
+    })
+}
+const showTabContends = (indexElement = 0) => {
+    tabContendBlocks[indexElement].style.display = 'block'
+    tabsBlocks[indexElement].classList.add('tab_content_item_active')
+}
+hideTabContend()
+showTabContends()
+
+tabsParentBlock.onclick = (event) => {
+    if (event.target.classList.contains('tab_content_item')){
+        tabsBlocks.forEach((tabBlock, tabIndex)=> {
+            if (event.target === tabBlock){
+                hideTabContend()
+                showTabContends(tabIndex)
+            }
+        })
+    }
+}
+let sliderIndex = 0
+const autoSlider = () => {
+    hideTabContend()
+    sliderIndex = (sliderIndex + 1) % tabContendBlocks.length
+    showTabContends(sliderIndex)
+}
+setInterval(autoSlider, 3000)
---
+++ js/modal.js
@@ -0,0 +1,31 @@
+// modal
+const modal = document.querySelector('.modal')
+const modalTrigger = document.querySelector('#btn-get')
+const modalCloseButton = document.querySelector('.modal_close')
+
+const openModel = () => {
+    modal.style.display = 'block'
+    document.body.style.overflow = 'hidden'
+}
+const closeModel = () => {
+    modal.style.display = 'none'
+    document.body.style.overflow = ''
+}
+modal.onclick = (event) => {
+    if (event.target === modal) {
+        closeModel()
+    }
+}
+modalTrigger.onclick = () => openModel()
+modalCloseButton.onclick = () => closeModel()
+
+const position = () => {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight ){
+      openModel()
+      window.removeEventListener('scroll', position)
+  }
+}
+window.addEventListener('scroll', position)
+
+
+setTimeout(()=> openModel(), 10000)
