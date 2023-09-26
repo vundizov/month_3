be8b43987f38fbd91aef186d3c3bc921ab82bc2b
1598256q <vundizovrustam07@gmail.com>
2023-09-26 21:50:01 +0600

    dz6

--- js/lesson.js
+++ js/lesson.js
@@ -93,6 +93,63 @@ converter(som,usd,eur,'som')
 converter(usd,som,eur,'usd')
 converter(eur,som,usd,'eur')
 
+// CARD SWITCHER
+const card = document.querySelector('.card')
+const btnNext = document.querySelector('#btn-next')
+const btnPrev = document.querySelector('#btn-prev')
+let count = 1
+
+// btnNext.onclick = ()=> {
+//     count++
+//     fetch(`https://jsonplaceholder.typicode.com/todos/${count}`)
+//         .then(response => response.json())
+//         .then(data => {
+//             card.innerHTML = `
+//             <p>${data.title}</p>
+//             <p style="color:${data.completed ? 'green' : 'red'}">${data.completed}</p>
+//             <span>${data.id}</span>
+//             `
+//
+//         })
+// }
+const switcher = () => {
+    fetch(`https://jsonplaceholder.typicode.com/todos/${count}`)
+        .then(response => response.json())
+        .then(data => {
+                card.innerHTML = `
+                <p>${data.title}</p>
+                <p style="color:${data.completed ? 'green' : 'red'}">${data.completed}</p>
+                <span>${data.id}</span>
+                `
+         })
+}
+switcher()
+
+    let next = ()=> {
+        if (count === 200) {
+            count = 1
+        } else {
+            count++
+        }
+        switcher()
+    }
+    let prev = ()=> {
+        if (count === 1) {
+            count = 200
+        } else {
+            count--
+        }
+        switcher()
+    }
+btnNext.onclick = next
+btnPrev.onclick = prev
+fetch(`https://jsonplaceholder.typicode.com/posts`)
+    .then(response => response.json())
+    .then((data)=> {
+        console.log(data)
+    })
+
+
 
 
 
